29c8cf86bc752d4d9620a2f8ac68470699f0c54e
Justas Lauzinskas <justaslau@gmail.com>
2022-06-22 15:34:20 -0400

    create product component, added react router v6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-4'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
 
       <Footer />
-    </>
+    </Router>
   );
 };
 
