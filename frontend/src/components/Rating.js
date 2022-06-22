2ba039a579a435c85055c41c2c262650dd4bfdeb
Justas Lauzinskas <justaslau@gmail.com>
2022-06-22 17:19:09 -0400

    created backend server, added axios to react

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -70,11 +70,4 @@ const Rating = ({ value, text, color }) => {
 Rating.defaultProps = {
   color: '#f8e825',
 };
-
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
-};
-
 export default Rating;
